aa6f9568ae7308d7f85780efba2b63c6de176b52
edith espinoza <edith_espino@hotmail.com>
2016-03-04 00:01:30 -0500

    archivo de edith

---
+++ index.php
@@ -0,0 +1,14 @@
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+<marquee><font color="fuchisia">
+<h1>eres una persona muy chevre gracias por tu amistad</h1>
+
+</marquee>
+</font>
+	<img src="imagenn2.jpg" alt="25">
+<h1>Para una persona muy especial osea tu!!!!!!</h1>
+</body>
+</html>
